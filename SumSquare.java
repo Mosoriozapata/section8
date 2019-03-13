05e57a57ea6f456f7068b758204b1c100b45b84f
brvwdrvw <48529537+brvwdrvw@users.noreply.github.com>
2019-03-13 15:06:22 -0400

    Update SumSquare.java

--- SumSquare.java
+++ SumSquare.java
@@ -1,4 +1,19 @@
 Public class SumSquare{
   Public static void main(String [] args){
-    
+    double num1 = 0;
+    double multiple = 3;
+    double sum = 0;
+    while(num1 < 12){
+    System.out.println(multiple);
+    System.out.println(sum);
+    double num = multiple * multiple;
+    sum = Math.pow(multiple,2) + num;
+    multiple += 3;
+    num1 += 1;
+    }
+    System.out.println(sum);
+
+
+  }
+}
   
